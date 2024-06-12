1c23b0362e35bf099b32a543958075d6216993fe
Vlad <vladvsavenko@gmail.com>
2024-06-12 09:09:20 +0300

    based alert

---
+++ src/options/options.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './options.css'
+
+function App() {
+  return <div>Hello World</div>
+}
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
